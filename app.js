ab89212647741d5f16a654691baf5949d9feee7e
Casey Panganiban <90519890+Okay-C@users.noreply.github.com>
2024-11-25 10:04:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const path = require('path');
 
 dotenv.config();
 
